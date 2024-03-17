ce7ab61d835b49fbf0a9a9d2ed0d4f3f9f4799b7
prabinkumarkf <prabinkumarkf@gmail.com>
2024-03-17 12:56:36 +0530

    priline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin"); 
+const plugin = require("tailwindcss/plugin")
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "./src/scripts/**/*.js",
-    "../node_modules/preline/dist/*.js",
+    "./node_modules/preline/dist/*.js",
+    "./node_modules/tw-elements/js/**/*.js"
   ],
 
   theme: {
@@ -14,14 +16,21 @@ module.exports = {
       transparent: "transparent",
       current: "currentColor",
       white: "#ffffff",
-      primary: "#081242",
-      secondary: "#4F8CE5",
-      para: "#808390",
+      main: "#081242",
+      sec: "#4F8CE5",
+      para: "#808390", 
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      yellow: colors.yellow,
+      blue: colors.blue,
+       
     },
     extend: {},
   },
   plugins: [ 
     require("@tailwindcss/forms"),
     require('preline/plugin'),
+    require("tw-elements/plugin.cjs"),
   ],
 };
