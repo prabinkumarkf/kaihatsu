853a08eccaf3e790fec0e505f42377c040b60adc
prabinkumarkf <prabinkumarkf@gmail.com>
2024-03-18 09:48:53 +0530

    hardware

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin")
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "./src/scripts/**/*.js",
-    "./node_modules/preline/dist/*.js", 
+    "./node_modules/preline/dist/*.js",
   ],
 
   theme: {
@@ -17,19 +17,14 @@ module.exports = {
       white: "#ffffff",
       main: "#081242",
       sec: "#4F8CE5",
-      para: "#808390", 
+      para: "#808390",
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
       yellow: colors.yellow,
       blue: colors.blue,
-       
     },
     extend: {},
   },
-  plugins: [ 
-    require("@tailwindcss/forms"),
-    require('preline/plugin'),
-    
-  ],
+  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
